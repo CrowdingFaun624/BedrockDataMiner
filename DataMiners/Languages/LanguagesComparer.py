757cb57e7a9cdcb10535c00daab1deee70f4f764
Andy Kovacs <anderson@kovax.org>
2024-01-24 19:46:44 -0500

    fix languages comparer resource packs

--- DataMiners/Languages/LanguagesComparer.py
+++ DataMiners/Languages/LanguagesComparer.py
@@ -2,19 +2,25 @@ from typing import TYPE_CHECKING
 
 import Comparison.Comparer as Comparer
 import DataMiners.DataMinerTyping as DataMinerTyping
+import Utilities.CollapseResourcePacks as CollapseResourcePacks
 
 if TYPE_CHECKING:
     import Utilities.Version as Version
     import DataMiners.DataMiner as DataMiner
 
 def normalize(data:DataMinerTyping.Languages, version:"Version.Version", dataminers:dict[str,"DataMiner.DataMinerCollection"]) -> DataMinerTyping.NormalizedLanguages:
-    def fix_properties(unfixed_data:DataMinerTyping.LanguagesTypedDict) -> dict[str,DataMinerTyping.LanguagesPropertiesTypedDict]:
+    def fix_properties(unfixed_data:DataMinerTyping.LanguagesTypedDict, resource_packs:DataMinerTyping.ResourcePacks) -> dict[str,DataMinerTyping.LanguagesPropertiesTypedDict]:
         output = unfixed_data["properties"]
         for resource_pack in unfixed_data["defined_in"]:
             if resource_pack not in output:
                 output[resource_pack] = {}
-        return output
-    return {language["code"]: fix_properties(language) for language in data}
+        return CollapseResourcePacks.collapse_resource_packs(output, resource_packs, version.name)
+
+    resource_packs:DataMinerTyping.ResourcePacks = dataminers["resource_packs"].get_data_file(version, non_exist_ok=True)
+    if resource_packs is None:
+        resource_packs = [{"name": "vanilla", "tags": ["core"], "id": 1}]
+
+    return {language["code"]: fix_properties(language, resource_packs) for language in data}
 
 def languages_comparison_move_function(key:str, value:DataMinerTyping.LanguagesTypedDict) -> dict[str,str]|None:
     output = {resource_pack_name: resource_pack_properties["name"] for resource_pack_name, resource_pack_properties in value.items() if "name" in resource_pack_properties}
@@ -22,7 +28,7 @@ def languages_comparison_move_function(key:str, value:DataMinerTyping.LanguagesT
 
 comparer = Comparer.Comparer(
     normalizer=normalize,
-    dependencies=None,
+    dependencies=["resource_packs"],
     base_comparer_section=Comparer.DictComparerSection(
         name="language",
         key_types=(str,),
@@ -34,13 +40,18 @@ comparer = Comparer.Comparer(
             name="resource pack",
             key_types=(str,),
             value_types=(dict,),
-            comparer=Comparer.DictComparerSection(
+            comparer=Comparer.TypedDictComparerSection(
                 name="property",
-                key_types=lambda key, value: key in "name",
-                value_types=lambda key, value: isinstance(value, {
-                    "name": (str,)
-                }[key]),
-                comparer=None
+                types=[
+                    ("name", str, None),
+                    ("defined_in", list, Comparer.ListComparerSection(
+                        name="resource pack",
+                        types=(str,),
+                        print_flat=True,
+                        ordered=False,
+                        comparer=None,
+                    ))
+                ]
             )
         )
     )
