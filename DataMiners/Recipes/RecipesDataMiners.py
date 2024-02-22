93ed781cbcf2fc4b884992a042d05796aa9591e3
Andy Kovacs <anderson@kovax.org>
2024-02-21 19:58:58 -0500

    recipes comparer

--- DataMiners/Recipes/RecipesDataMiners.py
+++ DataMiners/Recipes/RecipesDataMiners.py
@@ -1,8 +1,7 @@
-import Comparison.Comparer as Comparer
 import DataMiners.DataMiner as DataMiner
-# import DataMiners.Recipes.RecipesComparer as RecipesComparer
+import DataMiners.Recipes.RecipesComparer as RecipesComparer
 import DataMiners.Recipes.RecipesDataMiner0 as RecipesDataMiner0
 
-dataminers = DataMiner.DataMinerCollection("recipes.json", "recipes", Comparer.default_comparer, [
+dataminers = DataMiner.DataMinerCollection("recipes.json", "recipes", RecipesComparer.comparer, [
     DataMiner.DataMinerSettings("-", "-", RecipesDataMiner0.RecipesDataMiner0, ["behavior_packs"], behavior_packs_location="behavior_packs"),
 ])
