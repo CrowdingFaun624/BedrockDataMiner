745d53da275730ceaee384d7a943398dbe66fd44
Andy Kovacs <anderson@kovax.org>
2024-02-24 23:58:33 -0500

    fix indentation

--- Utilities/Sorting.py
+++ Utilities/Sorting.py
@@ -6,14 +6,14 @@ b = TypeVar("b")
 def sort_everything(data:a) -> a:
     '''Sorts the parent dictionary and all subdictionaries (recursive).'''
     if isinstance(data, list):
-            output = []
-            for item in data:
-                output.append(sort_everything(item))
-            return output
-        elif isinstance(data, dict):
-            return {key: sort_everything(value) for key, value in sorted(data.items())}
-        else:
-            return data
+        output:list[a] = []
+        for item in data:
+            output.append(sort_everything(item))
+        return output
+    elif isinstance(data, dict):
+        return {key: sort_everything(value) for key, value in sorted(data.items())}
+    else:
+        return data
 
 def sort_dict(data:dict[a,b]) -> dict[a,b]:
     return {key: value for key, value in sorted(data.items())}
