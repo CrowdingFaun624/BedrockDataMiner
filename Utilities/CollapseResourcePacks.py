b9b3e42021c8ee314d46bae23f2aec2a495a26b7
Andy Kovacs <anderson@kovax.org>
2024-03-01 22:10:55 -0500

    additional list capabilities

--- Utilities/CollapseResourcePacks.py
+++ Utilities/CollapseResourcePacks.py
@@ -9,8 +9,16 @@ def make_interface(has_defined_in_key:bool=True, pack_key:str="resource_packs") 
     def collapse_resource_packs_interface(data:dict[str,a], dependencies:"DataMinerTyping.DependenciesTypedDict") -> None:
         resource_packs = dependencies[pack_key]
         if resource_packs is None:
-            resource_packs = [{"name": "vanilla", "tags": ["core"], "id": 1}]
+            resource_packs:DataMinerTyping.ResourcePacks = [{"name": "vanilla", "tags": ["core"], "id": 1}]
         collapse_resource_packs(data, resource_packs, has_defined_in_key)
+    return collapse_resource_packs_interface
+
+def make_interface_list(pack_key:str="resource_packs") -> Callable[[list[str],"DataMinerTyping.DependenciesTypedDict"],None]:
+    def collapse_resource_packs_interface(data:list[str], dependencies:"DataMinerTyping.DependenciesTypedDict") -> None:
+        resource_packs = dependencies[pack_key]
+        if resource_packs is None:
+            resource_packs:DataMinerTyping.ResourcePacks = [{"name": "vanilla", "tags": ["core"], "id": 1}]
+        collapse_resource_pack_list(data, resource_packs)
     return collapse_resource_packs_interface
 
 def collapse_resource_packs(data:dict[str,a], resource_packs:list["DataMinerTyping.ResourcePackTypedDict"], add_defined_in:bool=True) -> dict[str,a]:
@@ -27,10 +35,14 @@ def collapse_resource_packs(data:dict[str,a], resource_packs:list["DataMinerTypi
         tags_str = ",".join(resource_pack["tags"])
         # tags_str is used as the new key
         if tags_str in output:
-            if isinstance(output[tags_str], dict):
-                output[tags_str].update(data[resource_pack_name])
-            else:
-                output[tags_str] = data[resource_pack_name]
+            output_location = output[tags_str]
+            match output_location:
+                case dict():
+                    output_location.update(data[resource_pack_name])
+                case list():
+                    output_location.extend(data[resource_pack_name])
+                case _:
+                    output[tags_str] = data[resource_pack_name]
             if add_defined_in:
                 output[tags_str]["defined_in"].append(resource_pack_name)
         else:
@@ -44,3 +56,22 @@ def collapse_resource_packs(data:dict[str,a], resource_packs:list["DataMinerTypi
         # tag has already been in here, so it assumes that and KeyErrors on defined_in
         data[key] = value
     return output
+
+def collapse_resource_pack_list(data:list[str], resource_packs:list["DataMinerTyping.ResourcePackTypedDict"]) -> list[str]:
+    resource_pack_names = {resource_pack["name"]: resource_pack for resource_pack in resource_packs}
+    for properties_resource_pack in data:
+        if properties_resource_pack not in resource_pack_names:
+            raise KeyError("Unknown resource pack \"%s\"!" % (properties_resource_pack))
+    output:list[str] = []
+    exists_in_output:set[str] = set()
+    while len(data) > 0:
+        properties_resource_pack = data.pop()
+        if properties_resource_pack not in resource_pack_names:
+            raise KeyError("Unknown resource pack \"%s\"!" % (properties_resource_pack))
+        tags_str = ",".join(resource_pack_names[properties_resource_pack]["tags"])
+        if tags_str not in exists_in_output:
+            output.append(tags_str)
+            exists_in_output.add(tags_str)
+    for item in output:
+        data.append(item)
+    return data
