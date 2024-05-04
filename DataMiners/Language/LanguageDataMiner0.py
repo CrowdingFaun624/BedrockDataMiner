e2ba62d6207a3407fe8d4d2048c84fe4336f5a1c
Andy Kovacs <anderson@kovax.org>
2024-05-04 17:32:04 -0400

    fix comment bug; move code to method

--- DataMiners/Language/LanguageDataMiner0.py
+++ DataMiners/Language/LanguageDataMiner0.py
@@ -16,6 +16,24 @@ class LanguageDataMiner0(LanguageDataMiner.LanguageDataMiner):
         self.location:str = kwargs["location"]
         self.file_display_name:str|None = kwargs["file_display_name"]
 
+    def process_line(self, line:str) -> tuple[str|None, DataMinerTyping.LanguageTypedDict|None]:
+        line = line.lstrip("\ufeff")
+        if len(line.lstrip()) == 0:
+            return None, None # empty line
+        if line.lstrip().startswith("#") or len(line) == 0:
+            return None, None # comment-only line, which I don't care about.
+        if "##" in line:
+            key_value, comment = line.split("##", maxsplit=1)
+        else:
+            key_value = line
+            comment = None
+        key_value = key_value.rstrip("\t")
+        key, value = key_value.split("=", maxsplit=1)
+        if comment is None:
+            return key, {"value": value}
+        else:
+            return key, {"comment": comment, "value": value}
+
     def activate(self, dependency_data:DataMinerTyping.DependenciesTypedDict) -> DataMinerTyping.Language:
         packs = dependency_data["resource_packs"]
         assert packs is not None
@@ -35,22 +53,16 @@ class LanguageDataMiner0(LanguageDataMiner.LanguageDataMiner):
             lines = language_file.splitlines()
             resource_pack_output:dict[str,DataMinerTyping.LanguageTypedDict] = {}
             for line in lines:
-                line = line.lstrip("\ufeff")
-                if len(line.lstrip()) == 0:
-                    continue # empty line
-                if line.lstrip().startswith("##") or len(line) == 0:
-                    continue # comment-only line, which I don't care about.
-                if "##" in line:
-                    key_value, comment = line.split("##", maxsplit=1)
-                else:
-                    key_value = line
-                    comment = None
-                key_value = key_value.rstrip("\t")
-                key, value = key_value.split("=", maxsplit=1)
-                if comment is None:
-                    resource_pack_output[key] = {"value": value}
-                else:
-                    resource_pack_output[key] = {"comment": comment, "value": value}
+                exception:Exception|None = None
+                try:
+                    key, line_data = self.process_line(line)
+                except Exception as e:
+                    exception = e
+                    exception.args = tuple(list(exception.args) + ["Failed to process line \"%s\" of pack \"%s\"!" % (line, pack_file)])
+                if exception is not None:
+                    raise exception
+                if key is None or line_data is None: continue
+                resource_pack_output[key] = line_data
             output[resource_pack_name] = resource_pack_output
 
         return Sorting.sort_everything(output)
