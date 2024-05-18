b0533631175c1378d5a085d0930abaa55fd47ce2
Andy Kovacs <anderson@kovax.org>
2024-05-18 16:37:35 -0400

    support for more, different messages

--- Structure/Structure.py
+++ Structure/Structure.py
@@ -27,36 +27,36 @@ class Structure(Generic[a]):
 
     def choose_structure_flat(self, key, value_type:type, value) -> Union["Structure",None]: ...
 
-    def print_single(self, key_str:str|int|None, data:a, message:str, output:list[SU.Line], printer:Union["Structure[a]",None]) -> list[Trace.ErrorTrace]:
+    def print_single(self, key_str:str|int|None, data:a, message:str, output:list[SU.Line], printer:Union["Structure[a]",None], post_message:str="") -> list[Trace.ErrorTrace]:
         exceptions:list[Trace.ErrorTrace] = []
         if printer is None:
             stringified_data = SU.stringify(data)
             if key_str is None:
-                output.append(SU.Line("%s %s %s.") % (message, self.field, stringified_data))
+                output.append(SU.Line("%s %s%s %s.") % (message, self.field, post_message, stringified_data))
             else:
-                output.append(SU.Line("%s %s %s of %s.") % (message, self.field, SU.stringify(key_str), stringified_data))
+                output.append(SU.Line("%s %s%s %s of %s.") % (message, self.field, post_message, SU.stringify(key_str), stringified_data))
         else:
             substructure_output, new_exceptions = printer.print_text(data)
             for exception in new_exceptions: exception.add(self.name, key_str)
             exceptions.extend(new_exceptions)
             match len(substructure_output), key_str is None:
                 case 0, True:
-                    output.append(SU.Line("%s empty %s.") % (message, self.field))
+                    output.append(SU.Line("%s empty %s%s.") % (message, self.field, post_message))
                 case 0, False:
-                    output.append(SU.Line("%s empty %s %s.") % (message, self.field, SU.stringify(key_str)))
+                    output.append(SU.Line("%s empty %s%s %s.") % (message, self.field, post_message, SU.stringify(key_str)))
                 case 1, True:
-                    output.append(SU.Line("%s %s %s.") % (message, self.field, substructure_output[0]))
+                    output.append(SU.Line("%s %s%s %s.") % (message, self.field, post_message, substructure_output[0]))
                 case 1, False:
-                    output.append(SU.Line("%s %s %s of %s.") % (message, self.field, SU.stringify(key_str), substructure_output[0]))
+                    output.append(SU.Line("%s %s%s %s of %s.") % (message, self.field, post_message, SU.stringify(key_str), substructure_output[0]))
                 case _, True:
-                    output.append(SU.Line("%s %s:") % (message, self.field))
+                    output.append(SU.Line("%s %s%s:") % (message, self.field, post_message))
                     output.extend(line.indent() for line in substructure_output)
                 case _, False:
-                    output.append(SU.Line("%s %s %s:") % (message, self.field, SU.stringify(key_str)))
+                    output.append(SU.Line("%s %s%s %s:") % (message, self.field, post_message, SU.stringify(key_str)))
                     output.extend(line.indent() for line in substructure_output)
         return exceptions
 
-    def print_double(self, key_str:str|int|None, data1:a, data2:a, message:str, output:list[SU.Line], printers:"StructureSet.StructureSet") -> list[Trace.ErrorTrace]:
+    def print_double(self, key_str:str|int|None, data1:a, data2:a, message:str, output:list[SU.Line], printers:"StructureSet.StructureSet", post_message:str="") -> list[Trace.ErrorTrace]:
         exceptions:list[Trace.ErrorTrace] = []
         substructure_output1, new_exceptions = printers.print_text(0, data1)
         for exception in new_exceptions: exception.add(self.name, key_str)
@@ -68,28 +68,28 @@ class Structure(Generic[a]):
         if len(substructure_output2) == 0: substructure_output2 = [SU.Line("empty")]
         match len(substructure_output1), len(substructure_output2), key_str is None:
             case 1, 1, True:
-                output.append(SU.Line("%s %s from %s to %s.") % (message, self.field, substructure_output1[0], substructure_output2[0]))
+                output.append(SU.Line("%s %s%s from %s to %s.") % (message, self.field, post_message, substructure_output1[0], substructure_output2[0]))
             case 1, 1, False:
-                output.append(SU.Line("%s %s %s from %s to %s.") % (message, self.field, SU.stringify(key_str), substructure_output1[0], substructure_output2[0]))
+                output.append(SU.Line("%s %s%s %s from %s to %s.") % (message, self.field, post_message, SU.stringify(key_str), substructure_output1[0], substructure_output2[0]))
             case 1, _, True:
-                output.append(SU.Line("%s %s from %s to:") % (message, self.field, substructure_output1[0]))
+                output.append(SU.Line("%s %s%s from %s to:") % (message, self.field, post_message, substructure_output1[0]))
                 output.extend(line.indent() for line in substructure_output2)
             case 1, _, False:
-                output.append(SU.Line("%s %s %s from %s to:") % (message, self.field, SU.stringify(key_str), substructure_output1[0]))
+                output.append(SU.Line("%s %s%s %s from %s to:") % (message, self.field, post_message, SU.stringify(key_str), substructure_output1[0]))
                 output.extend(line.indent() for line in substructure_output2)
             case _, 1, True:
-                output.append(SU.Line("%s %s to %s from:") % (message, self.field, substructure_output2[0]))
+                output.append(SU.Line("%s %s%s to %s from:") % (message, self.field, post_message, substructure_output2[0]))
                 output.extend(line.indent() for line in substructure_output1)
             case _, 1, False:
-                output.append(SU.Line("%s %s %s to %s from:") % (message, self.field, SU.stringify(key_str), substructure_output2[0]))
+                output.append(SU.Line("%s %s%s %s to %s from:") % (message, self.field, post_message, SU.stringify(key_str), substructure_output2[0]))
                 output.extend(line.indent() for line in substructure_output1)
             case _, _, True:
-                output.append(SU.Line("%s %s from:") % (message, self.field))
+                output.append(SU.Line("%s %s%s from:") % (message, self.field, post_message))
                 output.extend(line.indent() for line in substructure_output1)
                 output.append(SU.Line("to:"))
                 output.extend(line.indent() for line in substructure_output2)
             case _, _, False:
-                output.append(SU.Line("%s %s %s from:") % (message, self.field, SU.stringify(key_str)))
+                output.append(SU.Line("%s %s%s %s from:") % (message, self.field, post_message, SU.stringify(key_str)))
                 output.extend(line.indent() for line in substructure_output1)
                 output.append(SU.Line("to:"))
                 output.extend(line.indent() for line in substructure_output2)
