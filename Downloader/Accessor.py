83ea2992f51500af6d3da40af456b6d866f2b776
Andy Kovacs <anderson@kovax.org>
2024-06-02 16:56:25 -0400

    fix indents

--- Downloader/Accessor.py
+++ Downloader/Accessor.py
@@ -26,25 +26,25 @@ class Accessor():
 
     def file_exists(self, name:str) -> bool:
         return self.manager.file_exists(self.modify_file_name(name))
-    
+
     def get_files_in(self, parent:str) -> list[str]:
         return [self.trim_file_name(file) for file in self.manager.get_files_in(self.modify_file_name(parent))]
-    
+
     def get_file_list(self) -> list[str]:
         return [self.trim_file_name(file) for file in self.manager.get_files_in(self.modify_file_name())]
 
     def get_full_file_list(self) -> list[str]:
         return self.manager.get_file_list()
-    
+
     @overload
     def read(self, file_name:str, mode:Literal["b"]) -> bytes: ...
     @overload
     def read(self, file_name:str, mode:Literal["t"]) -> str: ...
     def read(self, file_name:str, mode:Literal["b","t"]="b") -> bytes|str:
         return self.manager.read(self.modify_file_name(file_name), mode)
-    
+
     def get_file(self, file_name:str, mode:Literal["b", "t"]="b") -> FileManager.FilePromise:
         return self.manager.get_file(self.modify_file_name(file_name), mode)
-    
+
     def all_done(self) -> None:
         return self.manager.all_done()
