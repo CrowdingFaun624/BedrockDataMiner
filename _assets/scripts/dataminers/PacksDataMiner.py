3dec31d0054517e3b9b79ecc76c3d333f5524cf4
Andy Kovacs <anderson@kovax.org>
2024-08-24 14:31:55 -0400

    subdirectory parameter

--- _assets/scripts/dataminers/PacksDataMiner.py
+++ _assets/scripts/dataminers/PacksDataMiner.py
@@ -14,16 +14,18 @@ class PackTypedDict(TypedDict):
 
 pack_order = collapse_resource_packs.resource_pack_order
 
-class PacksDataMiner(FileDataMiner.FileDataMiner):
+class PacksDataMiner(DataMiner.DataMiner):
 
     parameters = TypeVerifier.TypedDictTypeVerifier(
         TypeVerifier.TypedDictKeyTypeVerifier("directory", "a str", True, str),
-        TypeVerifier.TypedDictKeyTypeVerifier("pack_type", "a str", True, TypeVerifier.EnumTypeVerifier({"resource", "behavior"}))
+        TypeVerifier.TypedDictKeyTypeVerifier("pack_type", "a str", True, TypeVerifier.EnumTypeVerifier({"resource", "behavior"})),
+        TypeVerifier.TypedDictKeyTypeVerifier("subdirectory", "a str", False, (str, type(None)))
     )
 
-    def initialize(self, directory:str, pack_type:Literal["resource", "behavior"]) -> None:
+    def initialize(self, directory:str, pack_type:Literal["resource", "behavior"], subdirectory:str|None=None) -> None:
         self.directory = directory
         self.pack_type:Literal["resource", "behavior"] = pack_type
+        self.subdirectory = subdirectory
 
     def activate(self, environment:DataMinerEnvironment.DataMinerEnvironment) -> list[PackTypedDict]:
         file_list:list[str] = environment.dependency_data.get("all_files", self)
@@ -34,14 +36,20 @@ class PacksDataMiner(FileDataMiner.FileDataMiner):
         for file in file_list:
             if file.startswith(self.directory):
                 if file.count("/") == 1: continue # random file in resource packs directory, such as "flipbook_textures.json" in a0.16.0_build3
-                name = file.split("/")[1]
+                split_file = file.split("/")
+                name = split_file[1]
+                has_subdirectory = self.subdirectory is not None and (len(split_file) >= 3 and split_file[2] == self.subdirectory)
                 if name == "": continue # relic in old archive.org minecraft-iOS versions. Just a file with no content (b'')
                 if name not in pack_order:
                     unrecognized_packs.add(name)
                     continue
                 if name in pack_names: continue # so they aren't recorded multiple times - wonky behavior
                 pack_names.add(name)
-                packs.append({"name": name, "path": "%s/%s/" % (self.directory, name)})
+                if has_subdirectory:
+                    path = "%s/%s/%s/" % (self.directory, name, self.subdirectory)
+                else:
+                    path = "%s/%s/" % (self.directory, name)
+                packs.append({"name": name, "path": path})
 
         if len(unrecognized_packs) > 0:
             raise Exceptions.UnrecognizedPackError(sorted(unrecognized_packs), self.pack_type, self)
