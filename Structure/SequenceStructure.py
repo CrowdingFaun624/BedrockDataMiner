afd1fbe54809e9a13e5ec5ef5377f3acf6215a6c
Andy Kovacs <anderson@kovax.org>
2024-11-09 21:19:17 -0500

    optimize memory usage of SequenceStructure

--- Structure/SequenceStructure.py
+++ Structure/SequenceStructure.py
@@ -1,5 +1,7 @@
-import enum
-from typing import Sequence, TypeVar
+from itertools import count, takewhile
+from typing import Callable, Sequence, TypeVar
+
+import numpy
 
 import Structure.AbstractIterableStructure as AbstractIterableStructure
 import Structure.Difference as D
@@ -10,21 +12,11 @@ import Utilities.Exceptions as Exceptions
 
 d = TypeVar("d")
 
-class Direction(enum.Enum):
-    '''
-    Utility class used to retrace the most efficient path given
-    by the Levenshtein algorithm.
-    '''
-    none = "."
-    "value not filled in/top left of edit matrix"
-    horizontal = ">"
-    "deletion"
-    vertical = "v"
-    "addition"
-    diagonal_substitution = "\\"
-    "substitution"
-    diagonal_no_change = "-"
-    "no change"
+NONE = 0
+HORIZONTAL = 1
+VERTICAL = 2
+DIAGONAL_SUBSTITUTION = 3
+DIAGONAL_NO_CHANGE = 4
 
 class SequenceStructure(AbstractIterableStructure.AbstractIterableStructure[d]):
     '''
@@ -39,20 +31,21 @@ class SequenceStructure(AbstractIterableStructure.AbstractIterableStructure[d]):
         self.substitution_cost = substitution_cost
 
     def get_similarity(self, data1: Sequence[d], data2: Sequence[d], environment: StructureEnvironment.ComparisonEnvironment, exceptions: list[Trace.ErrorTrace]) -> float:
-        pass
-        if self.structure is None:
-            raise Exceptions.AttributeNoneError("structure", self)
-        distances:list[list[float]] = [[0] * (len(data1) + 1) for y in range(len(data2) + 1)]
+        if len(data1) * len(data2) > 10_000_000:
+            raise Exceptions.SequenceTooLongError(self, len(data1), len(data2))
+        similarity_function = self.structure.get_similarity if self.structure is not None else lambda data1, data2, environment, exceptions: float(data1 == data2)
+        distances = numpy.zeros((len(data2) + 1, len(data1) + 1), numpy.float32)
+        # distances:list[list[float]] = [[0] * (len(data1) + 1) for y in range(len(data2) + 1)]
         for x in range(1, len(data1) + 1):
-            distances[0][x] = x
+            distances[0, x] = x
         for y in range(1, len(data2) + 1):
-            distances[y][0] = y
+            distances[y, 0] = y
         for y in range(len(data2)):
             for x in range(len(data1)):
-                distances[y + 1][x + 1] = min(
-                    distances[y + 1][x] + 1,
-                    distances[y][x + 1] + 1,
-                    distances[y][x] + self.structure.get_similarity(data1[x], data2[y], environment, exceptions),
+                distances[y + 1, x + 1] = min(
+                    distances[y + 1, x] + 1,
+                    distances[y, x + 1] + 1,
+                    distances[y, x] + similarity_function(data1[x], data2[y], environment, exceptions),
                 )
         levenshtein_distance = distances[len(data2)][len(data1)]
         max_length = len(data1) if len(data1) > len(data2) else len(data2)
@@ -66,71 +59,82 @@ class SequenceStructure(AbstractIterableStructure.AbstractIterableStructure[d]):
     ) -> tuple[Sequence[d|D.Diff], bool, list[Trace.ErrorTrace]]:
         if data1 is data2 or data1 == data2:
             return data1, False, []
+        if len(data1) * len(data2) > 10_000_000:
+            raise Exceptions.SequenceTooLongError(self, len(data1), len(data2))
         exceptions:list[Trace.ErrorTrace] = []
-        if self.structure is None:
-            raise Exceptions.AttributeNoneError("structure", self)
+        similarity_function = self.structure.get_similarity if self.structure is not None else lambda data1, data2, environment, exceptions: float(data1 == data2)
+        compare_function:Callable[[d,d,StructureEnvironment.ComparisonEnvironment],tuple[d|D.Diff[d,d],bool,list[Trace.ErrorTrace]]] = \
+            self.structure.compare if self.structure is not None else lambda data1, data2, environment: (data1, False, []) if data1 is data2 or data1 == data2 else (D.Diff(old=data1, new=data2), True, [])
 
         data1_hashes:list[int] = [Hashing.hash_data(item) for item in data1]
         data2_hashes:list[int] = [Hashing.hash_data(item) for item in data2]
-        distances:list[list[float]] = [[0] * (len(data1) + 1) for y in range(len(data2) + 1)]
-        path:list[list[Direction]] = [[Direction.none] * (len(data1) + 1) for y in range(len(data2) + 1)]
-        for x in range(1, len(data1) + 1):
-            distances[0][x] = x
-            path[0][x] = Direction.horizontal
-        for y in range(1, len(data2) + 1):
-            distances[y][0] = y
-            path[y][0] = Direction.vertical
-        for y in range(len(data2)):
-            for x in range(len(data1)):
-                deletion_cost = distances[y + 1][x] + self.deletion_cost
-                addition_cost = distances[y][x + 1] + self.addition_cost
-                if data1_hashes[x] == data2_hashes[y]:
+        
+        prefix_len = sum(1 for i in takewhile(lambda a: a[0] == a[1], zip(data1_hashes, data2_hashes))) # number of items at start that are the same
+        shorter_length = min(len(data1), len(data2))
+        suffix_len = sum(1 for i in takewhile(lambda a: a[0] < shorter_length - prefix_len and a[1] == a[2], zip(count(), reversed(data1_hashes), reversed(data2_hashes)))) # number of items at end that are the same unless that line is included in prefix_len.
+        
+        distances = numpy.zeros((len(data2) + 1 - prefix_len - suffix_len, len(data1) + 1 - prefix_len - suffix_len), numpy.float32)
+        # distances:list[list[float]] = [[0] * (len(data1) + 1) for y in range(len(data2) + 1)]
+        path = numpy.zeros((len(data2) + 1 - prefix_len - suffix_len, len(data1) + 1 - prefix_len - suffix_len), numpy.int8)
+        # path:list[list[Direction]] = [[Direction.none] * (len(data1) + 1) for y in range(len(data2) + 1)]
+        for x in range(1, len(data1) + 1 - prefix_len - suffix_len):
+            distances[0, x] = x
+            path[0, x] = HORIZONTAL
+        for y in range(1, len(data2) + 1 - prefix_len - suffix_len):
+            distances[y, 0] = y
+            path[y, 0] = VERTICAL
+        for y in range(len(data2) - prefix_len - suffix_len):
+            for x in range(len(data1) - prefix_len - suffix_len):
+                deletion_cost:float = distances[y + 1, x] + self.deletion_cost
+                addition_cost:float = distances[y, x + 1] + self.addition_cost
+                if data1_hashes[x + prefix_len] == data2_hashes[y + prefix_len]:
                     substitution_cost = 0
-                    substitution_direction = Direction.diagonal_no_change
+                    substitution_direction = DIAGONAL_NO_CHANGE
                 else:
-                    substitution_cost = (1 - self.structure.get_similarity(data1[x], data2[y], environment, exceptions)) * self.substitution_cost
-                    substitution_direction = Direction.diagonal_substitution
+                    substitution_cost = (1 - similarity_function(data1[x + prefix_len], data2[y + prefix_len], environment, exceptions)) * self.substitution_cost
+                    substitution_direction = DIAGONAL_SUBSTITUTION
                 if substitution_cost < addition_cost and substitution_cost < deletion_cost:
                     cost = substitution_cost
                     direction = substitution_direction
                 elif addition_cost < substitution_cost and addition_cost < deletion_cost:
                     cost = addition_cost
-                    direction = Direction.vertical
+                    direction = VERTICAL
                 else:
                     cost = deletion_cost
-                    direction = Direction.horizontal
-                path[y + 1][x + 1] = direction
-                distances[y + 1][x + 1] = cost
-
+                    direction = HORIZONTAL
+                path[y + 1, x + 1] = direction
+                distances[y + 1, x + 1] = cost
         # retrace steps
         has_changes = False
         output:list[d|D.Diff] = []
-        x = len(data1) # x and y represent (at first) the bottom right corner of an edit distance matrix.
-        y = len(data2)
+        x = len(data1) - prefix_len - suffix_len # x and y represent (at first) the bottom right corner of an edit distance matrix.
+        y = len(data2) - prefix_len - suffix_len
         while x != 0 or y != 0:
-            match path[y][x]:
-                case Direction.diagonal_no_change:
-                    x -= 1; y -= 1
-                    output.append(data1[x]) # data1[x] and data2[y] must be equal
-                case Direction.diagonal_substitution:
-                    has_changes = True
-                    x -= 1; y -= 1
-                    comparison, any_changes, new_exceptions = self.structure.compare(data1[x], data2[y], environment)
-                    has_changes = has_changes or any_changes
-                    exceptions.extend(exception.add(self.name, None) for exception in new_exceptions)
-                    output.append(comparison)
-                case Direction.vertical:
-                    has_changes = True
-                    y -= 1
-                    output.append(D.Diff(new=data2[y]))
-                case Direction.horizontal:
-                    has_changes = True
-                    x -= 1
-                    output.append(D.Diff(old=data1[x]))
-                case Direction.none:
-                    raise Exceptions.InvalidStateError("Direction.none in middle of table!")
+            path_item:int = path[y, x]
+            if path_item == DIAGONAL_NO_CHANGE:
+                x -= 1; y -= 1
+                output.append(data1[x + prefix_len]) # data1[x] and data2[y] must be equal
+            elif path_item == DIAGONAL_SUBSTITUTION:
+                has_changes = True
+                x -= 1; y -= 1
+                comparison, any_changes, new_exceptions = compare_function(data1[x + prefix_len], data2[y + prefix_len], environment)
+                has_changes = has_changes or any_changes
+                exceptions.extend(exception.add(self.name, None) for exception in new_exceptions)
+                output.append(comparison)
+            elif path_item == VERTICAL:
+                has_changes = True
+                y -= 1
+                output.append(D.Diff(new=data2[y + prefix_len]))
+            elif path_item == HORIZONTAL:
+                has_changes = True
+                x -= 1
+                output.append(D.Diff(old=data1[x + prefix_len]))
+            else:
+                raise Exceptions.InvalidStateError("NONE in middle of table!")
         output.reverse() # the above process starts at the end of the sequence and goes backward
-        
+
+        output = data1[:prefix_len] + output + data1[len(data1)-suffix_len:] # type: ignore
+
         if type(data1) == list:
             actual_output:Sequence[d|D.Diff] = output
         else:
