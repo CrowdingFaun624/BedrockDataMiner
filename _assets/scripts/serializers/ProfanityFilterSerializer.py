5cf40116d870f10898975d2dc89fc1c0c7b0ff30
Andy Kovacs <anderson@kovax.org>
2024-11-09 20:50:22 -0500

    Profanity filter update in 1.21.50.25

---
+++ _assets/scripts/serializers/ProfanityFilterSerializer.py
@@ -0,0 +1,10 @@
+import base64
+
+import Serializer.Serializer as Serializer
+
+__all__ = ["ProfanityFilterSerializer"]
+
+class ProfanityFilterSerializer(Serializer.Serializer[list[str],list[str]]):
+    
+    def deserialize(self, data: bytes) -> list[str]:
+        return base64.b64decode(data).decode().splitlines()
