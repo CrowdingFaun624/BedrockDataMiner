4ccdf4480da1e9f3c94ab0e8fd530eef97d3eec3
Andy Kovacs <anderson@kovax.org>
2024-11-16 23:16:10 -0500

    Fix glyph_sizes being 2GB in size

--- _assets/scripts/serializers/GlyphSizesSerializer.py
+++ _assets/scripts/serializers/GlyphSizesSerializer.py
@@ -1,10 +1,13 @@
 from typing import TypedDict
 
+from typing_extensions import NotRequired
+
 import Serializer.Serializer as Serializer
 
 __all__ = ["GlyphSizesSerializer"]
 
 class RangeTypedDict(TypedDict):
+    character: NotRequired[str]
     left: int
     right: int
 
@@ -16,7 +19,14 @@ class GlyphSizesSerializer(Serializer.Serializer[dict[str,RangeTypedDict], dict[
             left_end = byte & 0x0F
             right_end = byte & 0xF0 >> 4
             assert right_end >= left_end
-            character = bytes(index).decode()
-            output[character] = {"left": left_end, "right": right_end}
+            character = chr(index)
+            try:
+                character.encode("UTF8")
+            except UnicodeEncodeError:
+                character = None
+            character_dict:RangeTypedDict = {"left": left_end, "right": right_end}
+            if character is not None:
+                character_dict["character"] = character
+            output[hex(index)[2:].upper().zfill(4)] = character_dict
         return output
             
