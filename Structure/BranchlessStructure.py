c0e1bed1757803e8758f82bbd3a68fec83276f3d
April Kovacs <anderson@kovax.org>
2025-06-03 01:14:14 -0400

    optimize BranchlessStructure

--- Structure/BranchlessStructure.py
+++ Structure/BranchlessStructure.py
@@ -1,3 +1,6 @@
+from types import EllipsisType
+from typing import Mapping
+
 import Structure.Container as Con
 import Structure.Difference as Diff
 import Structure.PassthroughStructure as PassthroughStructure
@@ -6,6 +9,7 @@ import Structure.StructureEnvironment as StructureEnvironment
 import Utilities.Exceptions as Exceptions
 import Utilities.Trace as Trace
 
+foo = False
 
 class BranchlessStructure[D, BO, CO](PassthroughStructure.PassthroughStructure[D, BO, CO]):
 
@@ -31,3 +35,34 @@ class BranchlessStructure[D, BO, CO](PassthroughStructure.PassthroughStructure[D
 
     def get_structure(self, data: D, trace: Trace.Trace, environment: StructureEnvironment.PrinterEnvironment) -> Structure.Structure[D, Con.Con[D], Con.Don[D], Con.Don[D]|Diff.Diff[Con.Don[D]], BO, CO]|None:
         return self.structure
+
+    def diffize(self, data: Con.Con[D], bundle: tuple[int, ...], trace: Trace.Trace, environment: StructureEnvironment.ComparisonEnvironment) -> Mapping[tuple[int,...],Con.Don[D]]|EllipsisType:
+        with trace.enter(self, self.name, data):
+            if self.structure is None:
+                return {bundle: data.as_don(bundle)}
+            else:
+                return self.structure.diffize(data, bundle, trace, environment)
+        return ...
+
+    def compare(self, datas: tuple[tuple[int, Con.Con[D]], ...], trace: Trace.Trace, environment: StructureEnvironment.ComparisonEnvironment) -> tuple[Con.Don[D]|Diff.Diff[Con.Don[D]]|EllipsisType, bool, bool]:
+        with trace.enter(self, self.name, datas):
+            consecutive_similarities = self.get_consecutive_similarities(datas, trace, environment)
+
+            if all(consecutive_similarity[4][1] for consecutive_similarity in consecutive_similarities): # if all datas are equal
+                diffized_output = self.diffize(datas[0][1], tuple(branch for branch, _ in datas), trace, environment)
+                return ... if diffized_output is ... else Diff.Diff(diffized_output, False), False, False
+
+            elif self.structure is None:
+                return Diff.Diff(self.get_without_structure_comparison(consecutive_similarities), False), True, False
+
+            else:
+                comparison, internal_changes, _ = self.structure.compare(datas, trace, environment)
+            if comparison is ...:
+                # error has occurred in `structure.compare`
+                return ..., False, False
+            elif isinstance(comparison, Diff.Diff):
+                return comparison, True, internal_changes
+            else:
+                return Diff.Diff({tuple(branch for branch, _ in datas): comparison}, internal_changes), True, internal_changes
+
+        return ..., False, False
