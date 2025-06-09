b9fac13573af4273a907116062f90df489eb2631
April Kovacs <anderson@kovax.org>
2025-06-09 10:50:33 -0400

    update language dataminers to not notice comments (too slow otherwise)

--- _domains/minecraft_bedrock/scripts/serializers/LanguageSerializer.py
+++ _domains/minecraft_bedrock/scripts/serializers/LanguageSerializer.py
@@ -1,63 +1,51 @@
-from itertools import takewhile
-from typing import Iterable, NotRequired, TypedDict
+from string import whitespace as py_whitespace
 
 from Serializer.Serializer import Serializer
 
 __all__ = ("LanguageSerializer",)
 
-class LanguageObject(TypedDict):
-    value: str
-    comment: NotRequired[str]
+LEFT_WHITESPACE = "\ufeff" + py_whitespace
+RIGHT_WHITESPACE = "#" + LEFT_WHITESPACE
 
-class LanguageSerializer(Serializer[dict[str,LanguageObject]]):
+class LanguageSerializer(Serializer[dict[str,str]]):
 
     empty_okay = True
 
-    def combine_lines(self, lines:list[str]) -> Iterable[str]:
+    def combine_lines(self, lines:list[str]) -> list[str]:
         current_str:str|None = None
+        output:list[str] = []
         for line in lines:
-            line = line.lstrip("\ufeff")
-            if len(line.lstrip()) == 0:
+            line = line.lstrip(LEFT_WHITESPACE).rstrip(RIGHT_WHITESPACE)
+            if len(line) == 0 or line.startswith("#"):
                 continue
-            if line.lstrip().startswith("#") or len(line) == 0:
-                continue
-            if current_str is None:
+            elif current_str is None:
                 current_str = line
                 continue
-            if "=" in line:
-                yield current_str
+            elif "=" in line:
+                output.append(current_str)
                 current_str = line
             else:
                 current_str += "\n" + line
+        if current_str is not None:
+            output.append(current_str)
+        return output
 
-    def process_line(self, line:str) -> tuple[str|None, LanguageObject|None]:
-        line = line.lstrip("\ufeff")
-        if len(line.lstrip()) == 0:
-            return None, None # empty line
-        if line.lstrip().startswith("#") or len(line) == 0:
-            return None, None # comment-only line, which I don't care about.
-        if "##" in line:
-            key_value, comment = line.split("##", maxsplit=1)
+    def process_line(self, line:str) -> tuple[str|None, str|None]:
+        if len(line) == 0 or line.startswith("#"):
+            return None, None # empty line or comment-only line
+        elif "##" in line:
+            key_value = line.split("##", maxsplit=1)[0]
+            key_value = key_value.rstrip("\t")
         else:
             key_value = line
-            comment = None
-        key_value = key_value.rstrip("\t")
         key, value = key_value.split("=", maxsplit=1)
-        if value.endswith("\t#"):
-            value = value.removesuffix("\t#").lstrip("\t")
-        if comment is not None:
-            octothorpe_count = sum(1 for octothorpe in takewhile(lambda char: char == "#", comment))
-            comment = comment[octothorpe_count:].lstrip() # I will destory ALL OCTOTHORPES HAHAHAHAHAHAHAaaAAAA
-        if comment is None:
-            return key, {"value": value}
-        else:
-            return key, {"comment": comment, "value": value}
+        return key, value
 
-    def deserialize(self, data: bytes) -> dict[str, LanguageObject]:
-        output:dict[str,LanguageObject] = {}
+    def deserialize(self, data: bytes) -> dict[str, str]:
+        output:dict[str,str] = {}
         lines = self.combine_lines(data.decode().splitlines())
         for line in lines:
-            key, line_data = self.process_line(line)
-            if key is None or line_data is None: continue
-            output[key] = line_data
+            key, value = self.process_line(line)
+            if key is None or value is None: continue
+            output[key] = value
         return output
