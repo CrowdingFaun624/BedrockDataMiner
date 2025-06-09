9b0e40bf3854edc2592973cc672c0610cab1f014
April Kovacs <anderson@kovax.org>
2025-06-09 11:12:49 -0400

    fix LatestVersionProvider dropping Versions.

--- Version/VersionProvider/LatestVersionProvider.py
+++ Version/VersionProvider/LatestVersionProvider.py
@@ -5,5 +5,14 @@ from Version.VersionProvider.VersionProvider import VersionProvider
 
 class LatestVersionProvider(VersionProvider):
 
+    def get_most_recent_version(self, all_versions:list[Version], index:int, supports_dataminer_collection:AbstractDataminerCollection) -> Version|None:
+        for i in range(index, -1, -1):
+            version = all_versions[i]
+            if supports_dataminer_collection.supports_version(version):
+                return version
+        else:
+            return None
+
     def get_versions(self, versions: list[Version], *, supports_dataminer_collection:AbstractDataminerCollection) -> list[Version]:
-        return [version for version in versions if version.latest and supports_dataminer_collection.supports_version(version)]
+        latest_versions = [self.get_most_recent_version(versions, index, supports_dataminer_collection) for index, version in enumerate(versions) if version.latest]
+        return sorted(set(version for version in latest_versions if version is not None))
