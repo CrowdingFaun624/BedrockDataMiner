6ec3da3f5960856a8c771ced16e2012535271a61
April Kovacs <anderson@kovax.org>
2025-06-21 13:14:37 -0400

    refactored AccessorComponent

--- Component/Accessor/AccessorComponent.py
+++ Component/Accessor/AccessorComponent.py
@@ -12,8 +12,11 @@ from Component.Field.ComponentField import ComponentField
 from Component.Field.Field import Field, InlinePermissions
 from Component.Pattern import Pattern
 from Downloader.Accessor import Accessor
+from Downloader.AccessorType import AccessorType
 from Utilities.Trace import Trace
 from Utilities.TypeVerifier import TypedDictKeyTypeVerifier, TypedDictTypeVerifier
+from Version.Version import Version
+from Version.VersionFile import VersionFile
 
 if TYPE_CHECKING:
     from Component.Version.VersionComponent import VersionComponent
@@ -25,48 +28,50 @@ class AccessorCreator():
 
     __slots__ = (
         "accessor",
-        "accessor_type_field",
+        "accessor_type",
         "arguments",
         "domain",
         "full_name",
-        "version_component",
-        "version_file_component",
+        "version",
+        "version_file",
     )
 
     def __init__(
         self,
         full_name:str,
         domain:"Domain.Domain",
-        version_file_component:"VersionFileComponent",
-        version_component:"VersionComponent",
-        accessor_type_field:ComponentField["AccessorTypeComponent"],
         arguments:dict[str,Any],
     ) -> None:
         self.full_name = full_name
         self.domain = domain
-        self.version_file_component = version_file_component
-        self.version_component = version_component
-        self.accessor_type_field = accessor_type_field
         self.arguments = arguments
+
         self.accessor:Accessor|None = None
+        self.accessor_type:AccessorType
+        self.version:Version
+        self.version_file:VersionFile
 
     def create_accessor(self, trace:Trace) -> Accessor|None:
         if self.accessor is None:
-            self.accessor = self.accessor_type_field.subcomponent.final.create_accessor(
+            self.accessor = self.accessor_type.create_accessor(
                 full_name=self.full_name,
                 trace=trace,
-                version=self.version_component.final,
+                version=self.version,
                 domain=self.domain,
-                file_type=self.version_file_component.version_file_type_field.subcomponent.final,
+                file_type=self.version_file.version_file_type,
                 instance_arguments=self.arguments
             )
         return self.accessor
 
-    def clear_variables(self) -> None:
-        '''Required step for being able to remove all Components after importing is finished.'''
-        del self.version_file_component
-        del self.version_component
-        del self.accessor_type_field
+    def link(
+        self,
+        accessor_type:AccessorType,
+        version:Version,
+        version_file:VersionFile,
+    ) -> None:
+        self.accessor_type = accessor_type
+        self.version = version
+        self.version_file = version_file
 
 class AccessorComponent(Component[AccessorCreator]):
 
@@ -92,12 +97,14 @@ class AccessorComponent(Component[AccessorCreator]):
         return AccessorCreator(
             self.full_name,
             self.domain,
-            cast("VersionFileComponent", self.get_inline_parent()), cast("VersionComponent", self.get_inline_parent().get_inline_parent()),
-            self.accessor_type_field, self.arguments
+            self.arguments
         )
 
-    def finalize(self, trace:Trace) -> None:
+    def link_finals(self, trace: Trace) -> None:
         with trace.enter(self, self.name, ...):
-            super().finalize(trace)
-            self.final.create_accessor(trace) # just test errors
-            self.final.clear_variables()
+            super().link_finals(trace)
+            self.final.link(
+                accessor_type=self.accessor_type_field.subcomponent.final,
+                version=self.get_inline_parent().get_inline_parent().final,
+                version_file=self.get_inline_parent().final,
+            )
