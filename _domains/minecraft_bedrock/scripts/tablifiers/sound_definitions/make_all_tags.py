6b68c1ab4477ca825310b76e03e171a82ffa646f
April Kovacs <anderson@kovax.org>
2025-08-17 22:54:56 -0400

    fix sound_definitions_tablifier

--- _domains/minecraft_bedrock/scripts/tablifiers/sound_definitions/make_all_tags.py
+++ _domains/minecraft_bedrock/scripts/tablifiers/sound_definitions/make_all_tags.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Mapping
 
 import _domains.minecraft_bedrock.scripts.normalizers.collapse_resource_packs as collapse_resource_packs
 from Component.ComponentFunctions import component_function
@@ -6,8 +6,10 @@ from Component.ComponentFunctions import component_function
 TAG_ORDER:dict[str,int] = {value.name: index for index, value in enumerate (collapse_resource_packs.ResourcePackTag)}
 
 @component_function()
-def make_all_tags(data:dict[str,dict[Any,Any]]) -> None:
+def make_all_tags(data:Mapping[str,Mapping[Any,Any]]) -> Mapping[str,Mapping[Any,Any]]:
     all_tag_combinations = sorted(set(",".join(resource_pack["tags"]) for resource_pack in collapse_resource_packs.resource_pack_dict.values()))
+    output:dict[str,Mapping[str,Any]] = {key: value for key, value in data.items()}
     for tag in all_tag_combinations:
         if tag not in data:
-            data[tag] = {}
+            output[tag] = {}
+    return output
